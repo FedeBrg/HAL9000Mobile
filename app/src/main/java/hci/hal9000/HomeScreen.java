2717a34e1fe8f278af07b7961fc52abb9b6e931b
FedeBrg <fede.bergagna@gmail.com>
2019-05-21 19:35:45 -0300

    Menu con iconos

--- app/src/main/java/hci/hal9000/HomeScreen.java
+++ app/src/main/java/hci/hal9000/HomeScreen.java
@@ -1,9 +1,13 @@
 package hci.hal9000;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -18,6 +22,31 @@ public class HomeScreen extends AppCompatActivity {
 
         String wel = "Welcome ";
         tv.setText(wel.concat(name).concat("!"));
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home_menu:
+                        Toast.makeText(HomeScreen.this, "home", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.devices_menu:
+                        Toast.makeText(HomeScreen.this, "devices", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.voice_menu:
+                        Toast.makeText(HomeScreen.this, "voice", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.routines_menu:
+                        Toast.makeText(HomeScreen.this, "routines", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rooms_menu:
+                        Toast.makeText(HomeScreen.this, "rooms", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
 
     }
 }
