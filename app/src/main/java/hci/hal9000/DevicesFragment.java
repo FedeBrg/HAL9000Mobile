c0ed2a9d7fd27bf319316579361c68a8eba96848
FedeBrg <fede.bergagna@gmail.com>
2019-05-23 14:08:54 -0300

    All bottom nav Fragments

---
+++ app/src/main/java/hci/hal9000/DevicesFragment.java
@@ -0,0 +1,17 @@
+package hci.hal9000;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DevicesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.devices_fragment, container, false);
+    }
+}
--- app/src/main/java/hci/hal9000/HomeScreen.java
+++ app/src/main/java/hci/hal9000/HomeScreen.java
@@ -3,6 +3,7 @@ package hci.hal9000;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -26,6 +27,13 @@ public class HomeScreen extends AppCompatActivity {
         String wel = "Welcome ";
         tv.setText(wel.concat(name).concat("!"));
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment newFragment = new HomeFragment();
+        fragmentTransaction.replace(R.id.container, newFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
 
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -34,27 +42,33 @@ public class HomeScreen extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment newFragment = new HomeFragment();
                 switch (item.getItemId()) {
                     case R.id.home_menu:
                         Toast.makeText(HomeScreen.this, "home", Toast.LENGTH_SHORT).show();
+                        newFragment = new HomeFragment();
                         break;
                     case R.id.devices_menu:
                         Toast.makeText(HomeScreen.this, "devices", Toast.LENGTH_SHORT).show();
+                        newFragment = new DevicesFragment();
                         break;
                     case R.id.voice_menu:
                         Toast.makeText(HomeScreen.this, "voice", Toast.LENGTH_SHORT).show();
+                        newFragment = new VoiceFragment();
                         break;
                     case R.id.routines_menu:
                         Toast.makeText(HomeScreen.this, "routines", Toast.LENGTH_SHORT).show();
+                        newFragment = new RoutinesFragment();
                         break;
                     case R.id.rooms_menu:
                         Toast.makeText(HomeScreen.this, "rooms", Toast.LENGTH_SHORT).show();
-                        RoomsFragment rf = new RoomsFragment();
-                        fragmentTransaction.replace(R.id.container, rf);
-                        fragmentTransaction.addToBackStack(null);
-                        fragmentTransaction.commit();
+                        newFragment = new RoomsFragment();
                         break;
+
                 }
+                fragmentTransaction.replace(R.id.container, newFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
                 return true;
             }
         });
